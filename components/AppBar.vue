d77553a70f7f58a9e2bc6c43857865752e46636d
olivercolas <olivercolas.oc@gmail.com>
2020-05-27 12:57:41 +0100

    refactor(components): created components to separate logic away from default.vue

---
+++ components/AppBar.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app-bar
+    :absolute="!windowScrollYGreaterThanZero"
+    :fixed="windowScrollYGreaterThanZero"
+    elevation="0"
+    hide-on-scroll
+    height="80px"
+    :style="{
+      background: themeBg,
+      borderBottom: appBarBorderBottom,
+      transition: 'all 0.1s'
+    }"
+    v-scroll="onScrollSetYPosition"
+  >
+    <Logo class="mobile-only" />
+    <LinkList class="not-mobile-only" :links="headerLinks.left" />
+    <v-spacer />
+    <LinkList class="not-mobile-only" :links="headerLinks.right" />
+    <button
+      type="button"
+      class="open-cart"
+      :data-count="cartCount"
+      @click="handleCartClick"
+    ></button>
+    <v-app-bar-nav-icon
+      class="mobile-only"
+      @click.stop="drawer = !drawer"
+      :style="{ marginLeft: '10px' }"
+    />
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { Getter } from 'vuex-class';
+import Logo from '~/components/Logo.vue';
+import LinkList from '~/components/LinkList.vue';
+
+@Component({
+  components: {
+    Logo,
+    LinkList
+  }
+})
+export default class AppBar extends Vue {
+  @Getter('cart/itemCount') cartCount: number;
+
+  headerLinks = {
+    left: [
+      {
+        text: 'Shop',
+        url: ''
+      },
+      {
+        text: 'Read',
+        url: ''
+      },
+      {
+        text: 'Stores',
+        url: ''
+      },
+      {
+        text: 'Search',
+        url: ''
+      }
+    ],
+    right: [
+      {
+        text: 'Login',
+        url: ''
+      }
+    ]
+  };
+
+  windowScrollYGreaterThanZero = false;
+
+  get themeBg() {
+    return this.$vuetify.theme.themes.light.background;
+  }
+
+  get appBarBorderBottom() {
+    return this.windowScrollYGreaterThanZero ? '1px solid #dfded4' : '0px';
+  }
+
+  onScrollSetYPosition() {
+    const scrollY =
+      window.scrollY || document.getElementsByTagName('html')[0].scrollTop;
+    if (scrollY > 0) {
+      this.windowScrollYGreaterThanZero = true;
+    } else {
+      this.windowScrollYGreaterThanZero = false;
+    }
+  }
+
+  handleCartClick() {
+    this.$store.dispatch('cart/toggleCart').then(result => {
+      if (!result) {
+        this.$store.dispatch('snackbar/open', {
+          message: 'You have no items in your cart',
+          type: null
+        });
+      }
+    });
+  }
+}
+</script>
---
+++ components/Snackbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-snackbar
+    :value="show"
+    :multiLine="multiLine"
+    :timeout="timeout"
+    :bottom="bottom"
+    :top="top"
+    :vertical="vertical"
+    :color="
+      color !== '' ? color : null
+    "
+    @click="close()"
+  >
+    <v-spacer></v-spacer>
+    {{ message }}
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { State, Action } from 'vuex-class';
+
+@Component
+export default class Snackbar extends Vue {
+  @State(state => state.snackbar.show || false) show: boolean;
+  @State(state => state.snackbar.multiLine) multiLine: boolean;
+  @State(state => state.snackbar.timeout) timeout: number;
+  @State(state => state.snackbar.bottom) bottom: boolean;
+  @State(state => state.snackbar.top) top: boolean;
+  @State(state => state.snackbar.vertical) vertical: boolean;
+  @State(state => state.snackbar.color) color: string;
+  @State(state => state.snackbar.message) message: string;
+  @Action('snackbar/close') close: Function
+}
+</script>
