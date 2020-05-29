980c6aad4ac6e24a1dc2437093727c0a762c459c
olivercolas <olivercolas.oc@gmail.com>
2020-05-29 14:30:25 +0100

    feat(mixin): added mixin for theme related functions

--- mixins/theme.ts
+++ mixins/theme.ts
@@ -1,8 +1,9 @@
-import { Vue, Component } from 'vue-property-decorator';
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
 
 @Component
 export default class Theme extends Vue {
   get themeBg() {
-    return this.$vuetify.theme.themes.light.background;
+    return (this as any).$vuetify.theme.themes.light.background;
   }
 }
