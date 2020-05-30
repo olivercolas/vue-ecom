9b9b64b16e2756bad4f5c13e80412d54d5aea5d4
olivercolas <olivercolas.oc@gmail.com>
2020-05-30 19:10:19 +0100

    refactor: removed unused method

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,58 +1,54 @@
-exports.getProduct = (req, res, next) => {
-	res.status(200).json({
-		product: { _id: '0' }
-	})
+const defaultProduct = {
+    _id: '0',
+    breadcrumbs: [
+        {
+            text: 'Skin',
+            url: ''
+        },
+        {
+            text: 'Cleanse',
+            url: ''
+        }
+    ],
+    title: 'In Two Minds Facial Cleanser',
+    description:
+        'A gentle gel-based formulation that cleanses thoroughly without drying the skin or stripping its natural oils.',
+    details: [
+        {
+            title: 'Suited to',
+            text: 'Combination skin'
+        },
+        {
+            title: 'Skin feel',
+            text: 'Purified, soft, refreshed'
+        },
+        {
+            title: 'Key ingredients',
+            text: 'Witch Hazel, Salicylic Acid, Sage Leaf'
+        }
+    ],
+    price: '23.00',
+    currency: 'GBP',
+    variants: [
+        {
+            sku: 'Cleanser100mL',
+            text: '100 mL',
+            image:
+                'https://www.aesop.com/medias/Aesop-Skin-In-Two-Minds-Facial-Cleanser-100mL-Small-455x524px.png?context=bWFzdGVyfGltYWdlc3wzNTI4MXxpbWFnZS9wbmd8aW1hZ2VzL2hkZi9oMDMvOTE0NzM0MjcxNjk1OC5wbmd8ZjVlMjMzOGY2NTdmZGU2NzA3MmExMTk4MjhmZTQ3ZDYwMDg2ODlkZjY1NWY3ZWUyNWM5M2RhMGI2MzIwNmMwNQ'
+        },
+        {
+            sku: 'Cleanser200mL',
+            text: '200 mL',
+            image:
+                'https://www.aesop.com/medias/Aesop-Skin-In-Two-Minds-Facial-Cleanser-200mL-Small-455x524px.png?context=bWFzdGVyfGltYWdlc3wxNTE5MTN8aW1hZ2UvcG5nfGltYWdlcy9oZjYvaDA3LzkwMTk3NzQ2MzE5NjYucG5nfDZkYTA4NGFmZDNkNzE1ZDc5MzgxZGUxMmIzMjBjMTYxYjg2NmNkZTQyOGU2NmEzNmMzZGMyNmIxNjBkMTlhNTY'
+        }
+    ]
 }
 
 exports.getAllProducts = (req, res, next) => {
-	res.status(200).json({
-		products: [
-			{
-				_id: '0',
-				breadcrumbs: [
-					{
-                        text: 'Skin',
-                        url: ''
-                    },
-                    {
-                        text: 'Cleanse',
-                        url: ''
-                    }
-				],
-				title: 'In Two Minds Facial Cleanser',
-				description:
-					'A gentle gel-based formulation that cleanses thoroughly without drying the skin or stripping its natural oils.',
-				details: [
-					{
-						title: 'Suited to',
-						text: 'Combination skin'
-					},
-					{
-						title: 'Skin feel',
-						text: 'Purified, soft, refreshed'
-					},
-					{
-						title: 'Key ingredients',
-						text: 'Witch Hazel, Salicylic Acid, Sage Leaf'
-					}
-				],
-				price: '23.00',
-				currency: 'GBP',
-				variants: [
-					{
-						sku: 'Cleanser100mL',
-						text: '100 mL',
-						image:
-							'https://www.aesop.com/medias/Aesop-Skin-In-Two-Minds-Facial-Cleanser-100mL-Small-455x524px.png?context=bWFzdGVyfGltYWdlc3wzNTI4MXxpbWFnZS9wbmd8aW1hZ2VzL2hkZi9oMDMvOTE0NzM0MjcxNjk1OC5wbmd8ZjVlMjMzOGY2NTdmZGU2NzA3MmExMTk4MjhmZTQ3ZDYwMDg2ODlkZjY1NWY3ZWUyNWM5M2RhMGI2MzIwNmMwNQ'
-					},
-					{
-						sku: 'Cleanser200mL',
-						text: '200 mL',
-						image:
-							'https://www.aesop.com/medias/Aesop-Skin-In-Two-Minds-Facial-Cleanser-200mL-Small-455x524px.png?context=bWFzdGVyfGltYWdlc3wxNTE5MTN8aW1hZ2UvcG5nfGltYWdlcy9oZjYvaDA3LzkwMTk3NzQ2MzE5NjYucG5nfDZkYTA4NGFmZDNkNzE1ZDc5MzgxZGUxMmIzMjBjMTYxYjg2NmNkZTQyOGU2NmEzNmMzZGMyNmIxNjBkMTlhNTY'
-					}
-				]
-			}
-		]
-	})
-}
+  res.status(200).json({
+    products: [
+      defaultProduct,
+    ],
+  });
+};
--- server/routes/products.js
+++ server/routes/products.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const ProductsControllers = require('../controllers/products')
 
-router.get('/:id', ProductsControllers.getProduct)
 router.get('', ProductsControllers.getAllProducts)
 
 module.exports = router
